021aadc22336d1419010e11e37547cabb4b3ae22
Socrates <108215275+0xSocrates@users.noreply.github.com>
2023-06-01 20:23:11 +0300

    Update empower-kurulum.sh

--- Empower Chain/empower-kurulum.sh
+++ Empower Chain/empower-kurulum.sh
@@ -58,7 +58,7 @@ rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.4.linux-amd64.tar.gz
 echo 'export GOROOT=/usr/local/go' >> $HOME/.bash_profile
 echo 'export GOPATH=$HOME/go' >> $HOME/.bash_profile
 echo 'export GO111MODULE=on' >> $HOME/.bash_profile
-echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile && . $HOME/.bash_profile
+echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile && source $HOME/.bash_profile
 rm -rf go1.20.4.linux-amd64.tar.gz
 exec > /dev/tty 2>&1
 echo -e '\e[0;32m✔'
@@ -141,6 +141,9 @@ sudo systemctl daemon-reload
 sudo systemctl enable empowerd
 sudo systemctl start empowerd
 sudo systemctl restart empowerd
+sleep 2
+source $HOME/.bash_profile
+systemctl restart empowerd
 exec > /dev/tty 2>&1
 echo -e ' '
 echo -e "\e[0;34mNode Başlatıldı\033[0m"
