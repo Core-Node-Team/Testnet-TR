53d518ee41b0d0a5a01f1e50ae1adacb90782f25
Socrates <108215275+0xSocrates@users.noreply.github.com>
2023-06-01 10:25:46 +0300

    Update empower-kurulum.sh

--- Empower Chain/empower-kurulum.sh
+++ Empower Chain/empower-kurulum.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 clear
+curl sSL https://raw.githubusercontent.com/0xSocrates/Testnet-Rehberler/main/Scripts/matrix.sh | bash
 sleep 1
-echo -e '\e[0;35m'
+echo -e '\e[0;32m'
 echo " ▄████████  ▄██████▄     ▄████████    ▄████████     ███▄▄▄▄    ▄██████▄  ████████▄     ▄████████ ";
 echo "███    ███ ███    ███   ███    ███   ███    ███     ███▀▀▀██▄ ███    ███ ███   ▀███   ███    ███ ";
 echo "███    █▀  ███    ███   ███    ███   ███    █▀      ███   ███ ███    ███ ███    ███   ███    █▀  ";
@@ -51,13 +52,13 @@ echo -e ''
 sleep 1
 echo -e "\e[0;34mGo Yükleniyor\033[0m"
 exec > /dev/null 2>&1
-cd
+cd $HOME
 wget https://go.dev/dl/go1.20.4.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.20.4.linux-amd64.tar.gz
-echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.4.linux-amd64.tar.gz
 echo 'export GOROOT=/usr/local/go' >> $HOME/.bash_profile
 echo 'export GOPATH=$HOME/go' >> $HOME/.bash_profile
-echo 'export GO111MODULE=on' >> $HOME/.bash_profile &&  . $HOME/.bash_profile
+echo 'export GO111MODULE=on' >> $HOME/.bash_profile
+echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile && . $HOME/.bash_profile
 rm -rf go1.20.4.linux-amd64.tar.gz
 exec > /dev/tty 2>&1
 echo -e '\e[0;32m✔'
@@ -70,7 +71,7 @@ exec > /dev/null 2>&1
 cd /$HOME
 git clone https://github.com/EmpowerPlastic/empowerchain.git
 cd empowerchain/chain
-git checkout v1.0.0-rc0
+git checkout v1.0.0-rc1
 make install 
 exec > /dev/tty 2>&1
 echo -e '\e[0;32m✔'
@@ -84,9 +85,9 @@ empowerd config chain-id circulus-1
 empowerd config keyring-backend test
 empowerd config node tcp://localhost:15057
 empowerd init $MONIKER --chain-id circulus-1
-curl -Ls https://raw.githubusercontent.com/0xSocrates/Testnet-Rehberler/main/EmpowerChain/genesis.json > $HOME/.empowerchain/config/genesis.json
+curl -Ls https://raw.githubusercontent.com/0xSocrates/Testnet-Rehberler/main/Empower%20Chain/genesis.json > $HOME/.empowerchain/config/genesis.json
 sleep 1
-curl -Ls https://raw.githubusercontent.com/0xSocrates/Testnet-Rehberler/main/EmpowerChain/addrbook.json > $HOME/.empowerchain/config/addrbook.json
+curl -Ls https://raw.githubusercontent.com/0xSocrates/Testnet-Rehberler/main/Empower%20Chain/addrbook.json > $HOME/.empowerchain/config/addrbook.json
 sleep 1
 seeds="d6a7cd9fa2bafc0087cb606de1d6d71216695c25@51.159.161.174:26656"
 peers="e8b3fa38a15c426e046dd42a41b8df65047e03d5@95.217.144.107:26656,89ea54a37cd5a641e44e0cee8426b8cc2c8e5dfb@51.159.141.221:26656,0747860035271d8f088106814a4d0781eb7b2bc7@142.132.203.60:27656,3c758d8e37748dc692621a0d59b454bacb69b501@65.108.224.156:26656,41b97fced48681273001692d3601cd4024ceba59@5.9.147.185:26656"
@@ -94,6 +95,8 @@ sed -i -e 's|^seeds *=.*|seeds = "'$seeds'"|; s|^persistent_peers *=.*|persisten
 sed -i.bak -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.025umpwr\"/" $HOME/.empowerchain/config/app.toml
 sed -i -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:15058\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:15057\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:15060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:15056\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":15066\"%" $HOME/.empowerchain/config/config.toml
 sed -i -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:15017\"%; s%^address = \":8080\"%address = \":15080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:15090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:15091\"%; s%:8545%:15045%; s%:8546%:15046%; s%:6065%:15065%" $HOME/.empowerchain/config/app.toml
+sed -i 's/max_num_inbound_peers =.*/max_num_inbound_peers = 50/g' $HOME/.empowerchain/config/config.toml
+sed -i 's/max_num_outbound_peers =.*/max_num_outbound_peers = 50/g' $HOME/.empowerchain/config/config.toml
 sed -i \
   -e 's|^pruning *=.*|pruning = "custom"|' \
   -e 's|^pruning-keep-recent *=.*|pruning-keep-recent = "100"|' \
@@ -151,7 +154,7 @@ echo -e "\e[0;34mKurulum Tamamlandı\e[0m\u2600"
 sleep 2 
 echo -e ""
 echo -e ""
-echo -e '\e[0;35m'
+echo -e '\e[0;32m'
 echo " ▄████████  ▄██████▄     ▄████████    ▄████████     ███▄▄▄▄    ▄██████▄  ████████▄     ▄████████ ";
 echo "███    ███ ███    ███   ███    ███   ███    ███     ███▀▀▀██▄ ███    ███ ███   ▀███   ███    ███ ";
 echo "███    █▀  ███    ███   ███    ███   ███    █▀      ███   ███ ███    ███ ███    ███   ███    █▀  ";
@@ -166,3 +169,4 @@ echo -e ""
 echo -e '\e[0m' 
 sleep 3
 curl -sSL https://raw.githubusercontent.com/0xSocrates/Testnet-Rehberler/main/Scripts/y%C4%B1ld%C4%B1z.sh | bash
+sleep 1
