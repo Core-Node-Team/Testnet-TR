d8a1f0821fdaa661ccc81b06dfeb6ca0026a8a81
Socrates <108215275+0xSocrates@users.noreply.github.com>
2023-08-03 23:41:15 +0300

    Update update-version-0.1.4.sh

--- Cascadia/update-version-0.1.3.sh
+++ Cascadia/update-version-0.1.3.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-
-sudo systemctl stop cascadiad
+clear
+echo -e "\e[0;34mStarting\033[0m"
+sleep 2
+exec > /dev/null 2>&1
+systemctl stop cascadiad
 cd $HOME
-curl -L https://github.com/CascadiaFoundation/cascadia/releases/download/v0.1.3/cascadiad-v0.1.3-linux-amd64 -o cascadiad
+curl -L https://github.com/CascadiaFoundation/cascadia/releases/download/v0.1.4/cascadiad-v0.1.4-linux-amd64 -o cascadiad
 chmod +x cascadiad
 sudo mv cascadiad $(which cascadiad)
+sudo systemctl start cascadiad
 sudo systemctl restart cascadiad
+exec > /dev/tty 2>&1
+echo -e "\e[0;34mCompleted\033[0m"
 sudo journalctl -u cascadiad -fo cat
