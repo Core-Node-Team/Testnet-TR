5e9ea7420810abee4d9aa9d43d9f538ff2815ba4
Socrates <108215275+0xSocrates@users.noreply.github.com>
2023-08-16 09:56:24 +0300

    Update babylon.sh

--- Babylon/babylon.sh
+++ Babylon/babylon.sh
@@ -26,11 +26,8 @@ exec > /dev/tty 2>&1
 }
 snapshot() {
 exec > /dev/null 2>&1
-sudo apt install liblz4-tool
-cp $HOME/.babylond/data/priv_validator_state.json $HOME/.babylond/priv_validator_state.json.backup 
-babylond tendermint unsafe-reset-all --home $HOME/.babylond --keep-addr-book 
+sudo apt install liblz4-tool -y
 curl -L http://128.140.4.67/CoreNode_Chain_Services/babylon_snapshot.tar.lz4 | tar -I lz4 -xf - -C $HOME/.babylond/data
-mv $HOME/.babylond/priv_validator_state.json.backup $HOME/.babylond/data/priv_validator_state.json 
 exec > /dev/tty 2>&1
 }
 init() {
