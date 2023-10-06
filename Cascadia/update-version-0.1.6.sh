1376cfe8b6408783d9b60393a73f6788a61fe72f
molla202 <91562185+molla202@users.noreply.github.com>
2023-10-06 16:14:57 +0300

    Create update-version-0.1.6.sh

---
+++ Cascadia/update-version-0.1.6.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+clear
+echo -e "\e[0;34mStarting\033[0m"
+sleep 2
+exec > /dev/null 2>&1
+systemctl stop cascadiad
+cd $HOME
+rm -rf cascadiad
+curl -L https://github.com/CascadiaFoundation/cascadia/releases/download/v0.1.6/cascadiad -o cascadiad
+chmod +x cascadiad
+sudo mv cascadiad $(which cascadiad)
+sudo systemctl start cascadiad
+sudo systemctl restart cascadiad
+exec > /dev/tty 2>&1
+clear
+rm -rf $HOME/cascadiad
+echo -e "\e[0;34mCompleted\033[0m"
+sudo journalctl -u cascadiad -fo cat
