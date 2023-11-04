9593f42ada3b5f22a0b51408f76c64a7b2c63ebe
molla202 <91562185+molla202@users.noreply.github.com>
2023-11-04 20:55:53 +0300

    Update upgrade-v0.13.0.sh

--- Elys/upgrade-v0.13.0.sh
+++ Elys/upgrade-v0.13.0.sh
@@ -3,16 +3,14 @@ clear
 echo -e "\e[0;34mStarting\033[0m"
 sleep 2
 exec > /dev/null 2>&1
-systemctl stop cascadiad
-cd $HOME
-rm -rf cascadiad
-curl -L https://github.com/CascadiaFoundation/cascadia/releases/download/v0.1.4/cascadiad -o cascadiad
-chmod +x cascadiad
-sudo mv cascadiad $(which cascadiad)
-sudo systemctl start cascadiad
-sudo systemctl restart cascadiad
+systemctl stop elysd
+cd $HOME/elys
+git fetch --all
+git checkout v0.13.0
+make build
+sudo mv $HOME/elys/build/elysd $(which elysd)
+sudo systemctl restart elysd
 exec > /dev/tty 2>&1
 clear
-rm -rf $HOME/cascadiad
 echo -e "\e[0;34mCompleted\033[0m"
-sudo journalctl -u cascadiad -fo cat
+sudo journalctl -u elysd -fo cat
