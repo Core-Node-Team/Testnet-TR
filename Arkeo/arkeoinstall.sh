205d27e4c56fc3682c9d1f4bae31146f3ae1f4e3
Socrates <108215275+0xSocrates@users.noreply.github.com>
2023-11-27 23:30:45 +0300

    Add files via upload

---
+++ Arkeo/arkeoinstall.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+clear
+BinaryName="arkeod"
+DirectName=".arkeo" #database directory
+CustomPort="314"
+NodeName="arkeo"  # project folder
+ChainID="arkeo"
+install_binary() {
+wget http://37.120.189.81/Arkeo/arkeod
+chmod +x arkeod
+mv arkeod $HOME/go/bin/
+}
+ge_ad_se_pe() {
+exec > /dev/null 2>&1
+curl -Ls https://raw.githubusercontent.com/Core-Node-Team/Testnet-TR/main/Arkeo/addrbook.json > $HOME/$DirectName/config/addrbook.json
+curl -Ls https://raw.githubusercontent.com/Core-Node-Team/Testnet-TR/main/Arkeo/genesis.json > $HOME/$DirectName/config/genesis.json
+peers="8c2d799bcc4fbf44ef34bbd2631db5c3f4619e41@213.239.207.175:60656,e46d22832e1746d099c7a0e329cee8d904337718@65.109.48.181:32656,0c96f2b20f856186dbff5dd2e85640aaae8a6034@5.181.190.76:22856,cb9401d70e1bd59e3ed279942ce026dae82aca1f@65.109.33.48:27656,465600bad30995e46124d5ec23021f4845be2ece@38.242.210.137:26656,32ec9022a9565e490bf28ce6550739156fd1e41e@81.0.221.203:26656,374facfe63ab4c786d484c2d7d614063190590b7@88.99.213.25:38656,1eaeb5b9cb2cc1ae5a14d5b87d65fef89998b467@65.108.141.109:17656,9c0df85008e400b440232fdb7470c593fa07609a@154.53.56.176:30656,65c95f70cf0ca8948f6ff59e83b22df3f8484edf@65.108.226.183:22856"
+seeds="0e1000e88125698264454a884812746c2eb4807@seeds.lavenderfive.com:22856"
+sed -i -e 's|^seeds *=.*|seeds = "'$seeds'"|; s|^persistent_peers *=.*|persistent_peers = "'$peers'"|' $HOME/$DirectName/config/config.toml
+# min gas price
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.0001uarkeo"|g' $HOME/.arkeo/config/app.toml
+sed -i 's|^prometheus *=.*|prometheus = true|' $HOME/.arkeo/config/config.toml
+exec > /dev/tty 2>&1
+}
+
+init() {
+$BinaryName config chain-id $ChainID
+$BinaryName config keyring-backend test
+$BinaryName config node tcp://localhost:${CustomPort}57
+$BinaryName init $MONIKER --chain-id $ChainID > $HOME/init.txt
+}
+
+config() {
+# set max in-out peers
+sed -i 's/max_num_inbound_peers =.*/max_num_inbound_peers = 50/g' $HOME/$DirectName/config/config.toml
+sed -i 's/max_num_outbound_peers =.*/max_num_outbound_peers = 50/g' $HOME/$DirectName/config/config.toml
+# puruning
+sed -i \
+  -e 's|^pruning *=.*|pruning = "custom"|' \
+  -e 's|^pruning-keep-recent *=.*|pruning-keep-recent = "100"|' \
+  -e 's|^pruning-keep-every *=.*|pruning-keep-every = "0"|' \
+  -e 's|^pruning-interval *=.*|pruning-interval = "10"|' \
+  $HOME/$DirectName/config/app.toml
+# indexer
+sed -i -e 's|^indexer *=.*|indexer = "null"|' $HOME/$DirectName/config/config.toml
+# custom port
+sed -i -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${CustomPort}58\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:${CustomPort}57\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${CustomPort}60\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:${CustomPort}56\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${CustomPort}66\"%" $HOME/$DirectName/config/config.toml
+sed -i -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${CustomPort}17\"%; s%^address = \":8080\"%address = \":${CustomPort}80\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:${CustomPort}90\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:${CustomPort}91\"%; s%:8545%:${CustomPort}45%; s%:8546%:${CustomPort}46%; s%:6065%:${CustomPort}65%" $HOME/$DirectName/config/app.toml
+sed -i -e "s%^address = \"tcp://localhost:1317\"%address = \"tcp://localhost:${CustomPort}17\"%; s%^address = \":8080\"%address = \":${CustomPort}80\"%; s%^address = \"localhost:9090\"%address = \"localhost:${CustomPort}90\"%; s%^address = \"localhost:9091\"%address = \"localhost:${CustomPort}91\"%; s%:8545%:${CustomPort}45%; s%:8546%:${CustomPort}46%; s%:6065%:${CustomPort}65%" $HOME/$DirectName/config/app.toml
+}
+
+get_moniker () {
+echo -e '\e[0;35m' && read -p "Moniker isminizi girin: " MONIKER 
+echo -e "\033[035mMoniker isminiz\033[034m $MONIKER \033[035molarak kaydedildi"
+echo -e '\e[0m'
+echo "export MONIKER=$MONIKER" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+sleep 2
+}
+
+removenode() {
+exec > /dev/null 2>&1
+sudo systemctl stop $BinaryName && \
+sudo systemctl disable $BinaryName && \
+rm /etc/systemd/system/$BinaryName.service && \
+sudo systemctl daemon-reload && \
+cd $HOME && \
+rm -rf $DirectName && \
+rm -rf $NodeName && \
+rm -rf $(which $BinaryName)
+exec > /dev/tty 2>&1
+}
+
+curl -sSL https://raw.githubusercontent.com/0xSocrates/Scripts/main/matrix.sh | bash
+curl -sSL https://raw.githubusercontent.com/0xSocrates/Scripts/main/core-node.sh | bash
+echo -e "\e[0;34m$NodeName Kurulumu Başlatılıyor\033[0m"
+sleep 2
+echo " "
+get_moniker
+echo -e "\e[0;34mSunucu Hazırlanıyor\033[0m"
+removenode
+curl -sSL https://raw.githubusercontent.com/0xSocrates/Scripts/main/preparing-server.sh | bash
+source $HOME/.bash_profile
+echo -e "\e[0;33mGüncellendi, Kütüphaneler Kuruldu, $(go version) Kuruldu\033[0m"
+sleep 2
+echo " "
+echo -e "\e[0;34m$BinaryName Kuruluyor\033[0m"
+install_binary
+echo -e "\e[0;33m$BinaryName $($BinaryName version) Kuruldu\033[0m"
+sleep 2
+init
+echo " "
+echo -e "\e[0;34m$BinaryName Başlatıldı\033[0m"
+sleep 2
+echo " "
+echo -e "\e[0;34mYapılandırma Dosyası Ayarları Yapılıyor\033[0m"
+config
+ge_ad_se_pe
+sleep 2
+echo -e "\e[0;33mTamamlandı\033[0m"
+exec > /dev/null 2>&1
+sudo tee /etc/systemd/system/$BinaryName.service > /dev/null <<EOF
+[Unit]
+Description=$NodeName Node
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=$(which $BinaryName) start --home $HOME/$DirectName
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target
+EOF
+sleep 2
+systemctl daemon-reload
+systemctl enable $BinaryName
+systemctl start $BinaryName
+systemctl restart $BinaryName
+exec > /dev/tty 2>&1
+echo " "
+echo -e "\e[0;34mNode Başlatıldı. Logları takip etmek için: \033[0;33m           sudo journalctl -u $BinaryName -fo cat\033[0m"
+sleep 2
+echo " "
+sleep 2
+source $HOME/.bash_profile
