40e1b4ad682b24cab07c7a933130b44201c8f72c
molla202 <91562185+molla202@users.noreply.github.com>
2024-07-30 04:41:22 +0300

    Create warden04.sh

---
+++ Warden-buenavista/warden04.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Hedef block yüksekliği
+TARGET_BLOCK=1534510
+
+while true; do
+    # Local node block yüksekliğini al
+    local_height=$(curl -s localhost:11257/status | jq -r .result.sync_info.latest_block_height)
+
+    # Mevcut block yüksekliğini ekrana yazdır
+    echo "Your node height: $local_height"
+
+    # Hedef block yüksekliğine ulaşıldı mı kontrol et
+    if [ "$local_height" -ge "$TARGET_BLOCK" ]; then
+        echo "Target block height reached. Updating app.toml and restarting services."
+
+        # app.toml dosyasını güncelle
+        wget -O $HOME/.warden/config/app.toml "https://raw.githubusercontent.com/Core-Node-Team/Testnet-TR/main/Warden-buenavista/app.toml"
+
+
+        # wardend servisini yeniden başlat
+        systemctl daemon-reload && sudo systemctl restart wardend && sudo systemctl restart slinkyd
+
+
+        # TMUX oturumunu kapat
+        tmux kill-session -t slinkycheck
+        break
+    fi
+
+    # 15 sn bekle
+    sleep 15
+done
